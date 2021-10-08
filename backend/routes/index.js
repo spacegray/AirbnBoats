81cbf72ab1dc757bed54407a26f2010b6f5f931a
Ayla Basha <aylabasha@gmail.com>
2021-10-08 15:45:07 -0700

    front end set up complete and tested

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,6 +4,14 @@ const apiRouter = require("./api");
 
 router.use("/api", apiRouter);
 
+// Add a XSRF-TOKEN cookie in development
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.json({});
+  });
+}
+
 // Hello World test route
 // router.get("/hello/world", function (req, res) {
 //   res.cookie("XSRF-TOKEN", req.csrfToken());

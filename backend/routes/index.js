121e690494e46329d3605e13c40e440320e489aa
Ayla Basha <aylabasha@gmail.com>
2021-10-08 14:27:46 -0700

    complete

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,10 +4,11 @@ const apiRouter = require("./api");
 
 router.use("/api", apiRouter);
 
-router.get("/hello/world", function (req, res) {
-  res.cookie("XSRF-TOKEN", req.csrfToken());
-  res.send("Hello World!");
-});
+// Hello World test route
+// router.get("/hello/world", function (req, res) {
+//   res.cookie("XSRF-TOKEN", req.csrfToken());
+//   res.send("Hello World!");
+// });
 
 module.exports = router;
 

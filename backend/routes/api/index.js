f0a9277ea4c1a13e6eba1601152f771877de6e1b
Ayla Basha <aylabasha@gmail.com>
2021-10-08 00:14:19 -0700

    phase 3 complete. need to test

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -19,5 +19,40 @@ router.post("/test", function (req, res) {
 //   .then((res) => res.json())
 //   .then((data) => console.log(data));
 
+// Test User Auth Middlewares
+// GET /api/set-token-cookie
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      },
+    })
+  setTokenCookie(res, user);
+  return res.json({ user });
+}));
+
+// Test
+// GET /api/restore-user
+const { restoreUser } = require('../../utils/auth.js');
+router.get(
+  '/restore-user',
+  restoreUser,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+// GET /api/require-auth
+const { requireAuth } = require('../../utils/auth.js');
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
 
 module.exports = router;

ad984eddf543d4ae24fc60f1093d3ea19c6ae45d
Ayla Basha <aylabasha@gmail.com>
2021-10-12 21:38:39 -0700

    boats and users migrated

--- backend/db/migrations/20211011181852-create-boat.js
+++ backend/db/migrations/20211011181852-create-boat.js
@@ -11,6 +11,7 @@ module.exports = {
       userId: {
         allowNull: false,
         type: Sequelize.INTEGER,
+        references: { model: demo-user }
       },
       name: {
         allowNull: false,

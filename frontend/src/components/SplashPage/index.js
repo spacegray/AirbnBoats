c6f9db04aba09f3b5dd5d0c4af557b91bb97c336
Ayla Basha <aylabasha@gmail.com>
2021-10-14 22:18:08 -0700

    little more styles

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -7,12 +7,12 @@ function SplashPage() {
     return (
       <>
         <div className="splash__container">
-          <img
-            className="splash"
-            src="https://cdn.pixabay.com/photo/2020/07/29/08/41/sailing-boat-5447438_1280.jpg"
-          ></img>
-          <div className="site__quote"> Your adventure awaits</div>
-        </div>
+            <img
+                className="splash"
+                src="https://cdn.pixabay.com/photo/2020/07/29/08/41/sailing-boat-5447438_1280.jpg"
+            ></img>
+            <div className="site__quote"> Your adventure awaits</div>
+            </div>
       </>
     );
 }

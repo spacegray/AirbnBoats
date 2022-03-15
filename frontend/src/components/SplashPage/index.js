6f3a8fc160654eb66a19d55f39faa018ff5f1741
Ayla Basha <aylabasha@gmail.com>
2022-03-15 00:57:32 -0700

    ...

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -2,18 +2,24 @@ import { Link } from 'react-router-dom';
 import './SplashPage.css';
 // const splashImage = require('../../images/splash-bkgrnd.jpg')
 import Footer from '../Footer/index'
-
+import ocean from './images/ocean.png'
 
 function SplashPage() {
 
     return (
       <>
         <div className="splash__container">
-          <div className="splash__image"></div>
-          <img
+          <div
+            className="splash__image"
+            styles={{ backgroundImage: `url(${ocean})` }}
+          
+            >
+          </div>
+
+          {/* <img
             className="splash"
             src="https://res.cloudinary.com/dexkxkrfp/image/upload/v1643094914/airbnboats/UNUSUAL-The-Colour-Of-The-Ocean-Will-Change-In-Eighty-Years-Due-To-Climate-Change_pl1fg7.jpg"
-          ></img>
+          ></img> */}
           <div className="site__quote"> Your adventure awaits</div>
         </div>
         <Footer />

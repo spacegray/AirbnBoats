63ba63cc7ff699412425f0e22d5a233e24fa2ff5
Ayla Basha <aylabasha@gmail.com>
2022-03-21 23:16:18 -0700

    added footer to listing page

--- frontend/src/components/BoatListingPage/index.js
+++ frontend/src/components/BoatListingPage/index.js
@@ -12,7 +12,7 @@ import {
 import { Modal } from "../../context/Modal";
 // import { ModalProvider } from "../../context/Modal";
 import  EditReviewForm from "../EditReviewForm/"
-
+import Footer from "../Footer";
 import "./BoatListingPage.css";
 
 function BoatListingPage() {
@@ -54,94 +54,106 @@ function BoatListingPage() {
   return (
     <>
       <div className="page__container">
-        <div className="boatList__section">
-          <div className="boat__image__section">
-            <div className="boat__image">
-              <img className="image" src={boat?.img} alt=""></img>
-            </div>
-          </div>
-          <div className="decription__container">
-            <div className="boat__details">
-              <h2>{boat?.name}</h2>
-              <div className="boat__address">
-                {boat?.address},{boat?.city}, {boat?.state} {boat?.country}{" "}
-                {boat?.zipCode}
-              </div>
-              <div className="price__section">
-                <div className="listing__price">$ {boat?.price} /day </div>
+        <div className="page__content">
+          <div className="boatList__section">
+            <div className="boat__image__section">
+              <div className="boat__image">
+                <img className="image" src={boat?.img} alt=""></img>
               </div>
             </div>
+            <div className="decription__container">
+              <div className="boat__details">
+                <h2>{boat?.name}</h2>
+                <div className="boat__address">
+                  {boat?.address},{boat?.city}, {boat?.state} {boat?.country}{" "}
+                  {boat?.zipCode}
+                </div>
+                <div className="price__section">
+                  <div className="listing__price">$ {boat?.price} /day </div>
+                </div>
+              </div>
 
-            <div className="review__table">
-              <h1>Reviews</h1>
-              <div className="reviewsList">
-                {sessionUser ? (
-                  <div className="form__section">
-                    {" "}
-                    <form className="review__form" onSubmit={handleSubmit}>
-                      <textarea
-                        className="review-area"
-                        type="text"
-                        placeholder="How was your experience?"
-                        value={review}
-                        onChange={(e) => setReview(e.target.value)}
-                      />
-                      <button className="submit__btn" type="submit">
-                        Submit
-                      </button>
-                    </form>{" "}
-                  </div>
-                ) : null}
+              <div className="review__table">
+                <h1>Reviews</h1>
+                <div className="reviewsList">
+                  {sessionUser ? (
+                    <div className="form__section">
+                      {" "}
+                      <form className="review__form" onSubmit={handleSubmit}>
+                        <textarea
+                          className="review-area"
+                          type="text"
+                          placeholder="How was your experience?"
+                          value={review}
+                          onChange={(e) => setReview(e.target.value)}
+                        />
+                        <button className="submit__btn" type="submit">
+                          Submit
+                        </button>
+                      </form>{" "}
+                    </div>
+                  ) : null}
 
-                {reviews ? (
-                  Object.keys(reviews).length === 0 ? (
-                    <h3>No reviews to display</h3>
-                  ) : (
-                    Object.values(reviews).map((eachReview) =>
-                      boat?.id === eachReview.boatId ? (
-                        <div
-                          key={eachReview.boatId}
-                          className="review__section"
-                        >
-                          {/* <h2>{eachReview.id}</h2> */}
-                          <h3 id="reviewer-name">
-                            {eachReview?.User?.username}
-                          </h3>
+                  {reviews ? (
+                    Object.keys(reviews).length === 0 ? (
+                      <h3>No reviews to display</h3>
+                    ) : (
+                      Object.values(reviews).map((eachReview) =>
+                        boat?.id === eachReview.boatId ? (
+                          <div
+                            key={eachReview.boatId}
+                            className="review__section"
+                          >
+                            {/* <h2>{eachReview.id}</h2> */}
+                            <h3 id="reviewer-name">
+                              {eachReview?.User?.username}
+                            </h3>
 
-                          <p id={`review-content ${eachReview.id}`}>{eachReview.review}</p>
-                          <p id="timestamp">{eachReview.createdAt}</p>
-                          {sessionUser &&
-                            sessionUser?.id === eachReview?.userId && (
-                              <>
-                                {/* <h1>{eachReview.review}</h1> */}
-                                <button
-                                  id="delete-review"
-                                  onClick={() =>
-                                    deleteReviewAlert(eachReview.id)
-                                  }
-                                >
-                                  <i className="trash__btn"> Delete</i>
-                                </button>
-                                <button onClick={() => {setShowModal(true); setReviewId(eachReview.id)}}>
-                                  Edit
-                                </button>
-                                {showModal && (
-                                  <Modal onClose={() => setShowModal(false)}>
-                                    <EditReviewForm data={{review_Id: reviewId}} />
-                                  </Modal>
-                                )}
-                              </>
-                            )}
-                        </div>
-                      ) : null
+                            <p id={`review-content ${eachReview.id}`}>
+                              {eachReview.review}
+                            </p>
+                            <p id="timestamp">{eachReview.createdAt}</p>
+                            {sessionUser &&
+                              sessionUser?.id === eachReview?.userId && (
+                                <>
+                                  {/* <h1>{eachReview.review}</h1> */}
+                                  <button
+                                    id="delete-review"
+                                    onClick={() =>
+                                      deleteReviewAlert(eachReview.id)
+                                    }
+                                  >
+                                    <i className="trash__btn"> Delete</i>
+                                  </button>
+                                  <button
+                                    onClick={() => {
+                                      setShowModal(true);
+                                      setReviewId(eachReview.id);
+                                    }}
+                                  >
+                                    Edit
+                                  </button>
+                                  {showModal && (
+                                    <Modal onClose={() => setShowModal(false)}>
+                                      <EditReviewForm
+                                        data={{ review_Id: reviewId }}
+                                      />
+                                    </Modal>
+                                  )}
+                                </>
+                              )}
+                          </div>
+                        ) : null
+                      )
                     )
-                  )
-                ) : null}
+                  ) : null}
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+      <Footer />
     </>
   );
 }
